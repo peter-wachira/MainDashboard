a0532d6d608e5e88db7213735a8131e0e860395a
Wazinsure PC <you@example.com>
2019-08-19 17:58:55 +0300

    add dashboard fragment layouts and animation for cards

--- app/src/main/java/com/wazinsure/maindashboard/MainActivity.java
+++ app/src/main/java/com/wazinsure/maindashboard/MainActivity.java
@@ -1,14 +1,34 @@
 package com.wazinsure.maindashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Dashboard instanciation
+        DashboardFrag dashboardFrag  = new DashboardFrag();
+
+        fragmentTransaction.replace(R.id.container,dashboardFrag);
+        fragmentTransaction.commit();
     }
 }
